7d8c188ae568ff609d16550d85db2b41897cad1c
Mykhailenko <juliam4433@gmail.com>
2022-09-16 16:07:13 +0200

    form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React, { Component } from "react";
+import { ContactForm } from './ContactForm/ContackForm';
+
+
+export class App extends Component  {
+  state = {
+  
+  };
+
+  render() {
+    const addContact = this.addContact;
+
+
+    return (
+  <div>
+      <h1>Phonebook</h1>
+       <ContactForm  onSubmit={addContact}/>
+
+      <h2>Contacts</h2>
+        {/* <Filter />
+        <ContactList /> */}
     </div>
-  );
+
+)
+  };
 };
+
+  
---
+++ src/components/ContactForm/ContackForm.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Formik, Form, Field } from 'formik';
+import PropTypes from 'prop-types';
+
+export const ContactForm = () => {
+    
+    const handleSubmit = (values, { resetForm }) => {
+        console.log(values);
+        resetForm();
+    };
+    
+    return (
+        <Formik
+            initialValues={{ name: '', number: '' }}
+            onSubmit={handleSubmit}
+        >
+            <Form >
+                <label>
+                    Name
+                    <Field
+                        type="text"
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                    />
+                </label>
+                <label>
+                    Number
+                    <Field
+                        type="tel"
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                    />
+                </label>
+                <button type="submit" >Add Contact</button>
+            </Form>
+        </Formik>
+        
+    );
+};
